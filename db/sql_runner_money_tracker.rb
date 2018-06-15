16482c35e64195648d231641daa4d5dc319e14fd
StuartCharters <charters.stuart@gmail.com>
2018-06-15 22:31:04 +0100

    initial commit

---
+++ db/sql_runner_money_tracker.rb
@@ -0,0 +1,32 @@
+require('PG')
+
+class SqlRunner
+
+  def self.run(sql, values =[])
+    begin
+      db = PG.connect({
+        dbname: 'money_tracker',
+        host: 'localhost'
+        })
+      db.prepare('query', sql)
+      results = db.exec_prepared('query', values)
+    ensure
+      db.close() if db != nil
+    end
+  return results
+  end
+
+  # def self.run_unsanitised(sql, values=[])
+  #   begin
+  #     db = PG.connect({
+  #       dbname: 'money_tracker',
+  #       host: 'localhost'
+  #       })
+  #     results = db.exec(sql, values)
+  #   ensure
+  #     db.close() if db != nil
+  #   end
+  #   return results
+  # end
+
+end
