33501ec1c1b1cd275e80af43b467a4727fb9d9c1
StuartCharters <charters.stuart@gmail.com>
2018-06-16 10:55:57 +0100

    can convert string integer to integer in pence

--- models/specs/trip_spec.rb
+++ models/specs/trip_spec.rb
@@ -10,14 +10,25 @@ class TripTest < MiniTest::Test
       "current" => "true",
       "timelog" => "2018-10-13"
       })
+
+    @trip2 = Trip.new({
+      "name" => "Family holiday",
+      "budget" => "50000",
+      "current" => "true",
+      "timelog" => "2018-10-13"
+      })
   end
 
   def test_has_name
     assert_equal("Family holiday", @trip1.name())
   end
 
-  def test_has_budget_in_pence
+  def test_has_budget_in_pence__given_in_decimal
     assert_equal(50_000, @trip1.budget())
+  end
+
+  def test_has_budget_in_pence__given_as_integer
+    assert_equal(50_000, @trip2.budget())
   end
 
 end
