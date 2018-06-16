f208295950b473a0147c1da57da645d903ed8a89
StuartCharters <charters.stuart@gmail.com>
2018-06-16 15:10:10 +0100

    setup views and public file structure

---
+++ money_tracker.rb
@@ -0,0 +1,13 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/item_controller')
+require_relative('controllers/person_controller')
+require_relative('controllers/person_trans_controller')
+require_relative('controllers/tag_controller')
+require_relative('controllers/trans_tag_controller')
+require_relative('controllers/transaction_controller')
+require_relative('controllers/trip_controller')
+
+get '/' do
+  erb( :index )
+end
