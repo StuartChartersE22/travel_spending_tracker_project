df4a94e7e72fedb9790ed98b81e1584fa9b8e1c4
StuartCharters <charters.stuart@gmail.com>
2018-06-16 15:22:05 +0100

    structured controllers with teh 7 restful routes

---
+++ controllers/transaction_controller.rb
@@ -0,0 +1,19 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+also_reload("../models/*")
+also_reload("../controllers/*")
+require_relative("../models/transaction.rb")
+
+#INDEX
+
+#CREATE
+
+#NEW
+
+#SHOW
+
+#EDIT
+
+#UPDATE
+
+#DELETE
--- money_tracker.rb
+++ money_tracker.rb
@@ -1,5 +1,3 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
 require_relative('controllers/item_controller')
 require_relative('controllers/person_controller')
 require_relative('controllers/person_trans_controller')
@@ -8,6 +6,7 @@ require_relative('controllers/trans_tag_controller')
 require_relative('controllers/transaction_controller')
 require_relative('controllers/trip_controller')
 
+#INDEX
 get '/' do
   erb( :index )
 end
