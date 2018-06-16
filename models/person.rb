7d2484be053719e594deca4fd89542a5b0da4706
StuartCharters <charters.stuart@gmail.com>
2018-06-16 09:22:11 +0100

    person class with initialize setup

---
+++ models/person.rb
@@ -0,0 +1,14 @@
+require("../db/sql_runner_money_tracker.rb")
+require("pry")
+
+class Trip
+
+  attr_reader(:id)
+  attr_accessor(:name)
+
+  def initialize(details)
+    @id = details["id"].to_i() if details["id"]
+    @name = details["name"]
+  end
+
+end
