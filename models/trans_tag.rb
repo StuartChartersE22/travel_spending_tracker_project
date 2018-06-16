301ab5cbecd8b5241d8cb01264af16c05ae9e261
StuartCharters <charters.stuart@gmail.com>
2018-06-16 09:40:35 +0100

    TansTag class with initialize setup

---
+++ models/trans_tag.rb
@@ -0,0 +1,14 @@
+require("../db/sql_runner_money_tracker.rb")
+require("pry")
+
+class TransTag
+
+  attr_reader(:id, :transaction_id, :tag_id)
+
+  def initialize(details)
+    @id = details["id"].to_i() if details["id"]
+    @transaction_id = details["transaction_id"].to_i()
+    @tag_id = details["tag_id"].to_i()
+  end
+
+end
