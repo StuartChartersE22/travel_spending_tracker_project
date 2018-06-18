9eb7b2b348ab8b8ccfab3b414420c2d1ab09a565
StuartCharters <charters.stuart@gmail.com>
2018-06-18 12:24:17 +0100

    can create a new owing from person view which doesn't increase the transaction amount

--- controllers/person_trans_controller.rb
+++ controllers/person_trans_controller.rb
@@ -4,23 +4,26 @@ also_reload("../models/*")
 also_reload("../controllers/*")
 require_relative("../models/person_trans.rb")
 
-#NEW FROM PERSON
+#NEW FROM PERSON FOR Transaction
 get "/person_trans/:id/person" do
   @person = Person.find(params["id"])
   erb(:"person_trans/new")
 end
 
-#CREATE FROM PERSON
+#CREATE FROM PERSON FOR TRANSACTION
 post "/person_trans/create/:person_id/person" do
   params["owe"] += ".00" if params["owe"].count(".") == 0
   @transaction = Transaction.find(params["transaction_id"])
   params["date"] = @transaction.date()
   @person_trans = PersonTrans.new(params)
   @person_trans.save()
-  @transaction.amount = @transaction.amount() + Money.convert_to_integer(params["owe"])
-  @transaction.update()
   redirect to("/person/#{params["person_id"]}")
 end
+
+#NEW FROM PERSON FOR TRIP
+
+
+#CREATE FROM PERSON FOR TRIP
 
 #SHOW
 
