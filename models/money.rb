7a221874f204eef650cf189ffa2cd50b45fa9e71
StuartCharters <charters.stuart@gmail.com>
2018-06-20 14:46:19 +0100

    trip new form is formatted the same as the edit

--- models/money.rb
+++ models/money.rb
@@ -10,11 +10,11 @@ class Money
 
   def self.convert_to_decimal_string(integer)
     if 0 <= integer && integer < 100
-      decimal_amount_as_string = "0.%02d" % [integer]
+      decimal_amount_as_string = "£ 0.%02d" % [integer]
     elsif 0 > integer && integer > -100
-      decimal_amount_as_string = "-0.%02d" % [integer]
+      decimal_amount_as_string = "£ -0.%02d" % [integer]
     else
-      decimal_amount_as_string = integer.to_s().insert(-3,".")
+      decimal_amount_as_string = "£ #{integer.to_s().insert(-3,'.')}"
     end
   end
 
